83752034f86180b4d6972e505a98c4cc3e3ed894
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-10-31 15:06:32 +0200

    feature: add routes to the app

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,21 +1,22 @@
+import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
   return (
     <nav className="flex justify-between my-4">
         <div className="">
             <ul className="flex gap-6 text-gray-900 font-semibold">
-                    <li className="">Neth BookPoint</li>
-                    <li className="">Home</li>
-                    <li className="">Shop</li>
-                    <li className="">About Us</li>
-                    <li className="">Contact Us Team</li>
+                    <NavLink to="/" className="">Neth BookPoint</NavLink>
+                    <NavLink to="/" className="">Home</NavLink>
+                    <NavLink to="/shop" className="">Shop</NavLink>
+                    <NavLink to="/about" className="">About Us</NavLink>
+                    <NavLink to="/contact" className="">Contact Us Team</NavLink>
             </ul>
         </div>
         <div className="">
         <ul className="flex gap-6 text-gray-900 font-semibold">
-                <li className="">Like</li>
-                <li className="">Cart</li>
-                <li className="">Get In Touch</li>
+                <NavLink to="/like" className="">Like</NavLink>
+                <NavLink to="/cart" className="">Cart</NavLink>
+                <NavLink to="/intouch" className="">Get In Touch</NavLink>
             </ul>
         </div>
     </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Navigation from '../components/navigation'
 
 export default function HomePage() {
   return (
-    <div>Home</div>
+    <Navigation/>
   )
 }
