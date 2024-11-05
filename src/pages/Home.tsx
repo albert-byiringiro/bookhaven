8bbe7c5c1e35beed5879383fe464311a66aa3eca
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-05 21:50:46 +0200

    feature: add a book card in the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import SearchBox from "../components/SearchBox";
+import BookCard from "../components/Shared/BookCard";
 
 export default function HomePage() {
   return (
+    <div className="">
     <div className="pt-12 space-y-8 grid justify-center content-center">
       <h1 className="font-semibold text-gray-800 text-4xl text-center">All Books</h1>
       <div className="">
         <SearchBox/>
       </div>
+    </div>
+      <main className="grid">
+      <BookCard/>
+      </main>
     </div>
   )
 }
